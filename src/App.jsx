019250b8fc9e686ef7bf1ebcd8df15ac18376e97
Nikola <dzonza996@gmail.com>
2024-12-02 10:28:41 +0100

    added bg-image to the main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import Sidebar from '/src/components/Sidebar/Sidebar';
 import MainEdit from '/src/components/MainEdit/MainEdit';
+import bgImage from '/src/assets/bg-image.jpg';
 function App() {
   return (
     <>
       <main>
+        <img src={bgImage} alt="background image" className="bg-image" />
         <Sidebar />
         <MainEdit />
       </main>
